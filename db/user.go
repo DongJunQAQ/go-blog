323c6de8cd3b84710d888505c68b3155dbbc3c6a
DongJun <you@example.com>
2024-08-23 15:51:55 +0800

    修改日志打印信息

--- db/user.go
+++ db/user.go
@@ -12,7 +12,7 @@ func GetUserByName(name string) *models.User {
 	var user models.User
 	if err := db.Where("name = ?", name).First(&user).Error; err != nil { //判断查询是否出错，如果有错再判断错误类型
 		if !errors.Is(gorm.ErrRecordNotFound, err) { //如果不是"数据库无数据"的错误则打印日志
-			utils.LogRus.Errorf("获取%v用户的数据失败:%s", user, err) //遇到了其他错误
+			utils.LogRus.Errorf("获取%s用户的数据失败:%s", name, err) //遇到了其他错误
 		}
 		return nil //如果是"数据库无数据"的错误则返回空值
 	}
