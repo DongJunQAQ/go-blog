38516f596bdc5086237f998b4098bfd2e1835079
DongJun <you@example.com>
2024-08-23 14:26:54 +0800

    初始化MySQL

--- models/user.go
+++ models/user.go
@@ -1,7 +1,11 @@
 package models
 
 type User struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement"`
+	ID       uint   `gorm:"primaryKey;autoIncrement"` //``被称为Tag
 	Name     string `gorm:"unique;size:20"`
 	Password string `gorm:"size:32"`
+}
+
+func (User) TableName() string { //设置自动创建的表名
+	return "user"
 }
