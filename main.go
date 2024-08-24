1cbcc5f9c4066f772ab8149975f26a05be94abc8
DongJun <you@example.com>
2024-08-24 21:43:44 +0800

    添加前端文件并引入

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"net/http"
 )
 
-func Home(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "home.html", nil)
+func LoginView(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "login.html", nil)
 }
 func main() {
 	router := gin.Default()
-	router.LoadHTMLFiles("view/home.html") //加载HTML文件
-	router.GET("/", Home)
+	router.Static("/js", "view/js")                                                  //当用户访问http://yourdomain/js时，服务器会返回view/js目录下的所有文件
+	router.StaticFile("/favicon.ico", "view/img/dqq.png")                            //当有请求访问/favicon.ico时，服务器会返回view/img/dqq.png这个文件
+	router.LoadHTMLFiles("view/login.html", "view/blog_list.html", "view/blog.html") //加载HTML文件
+	router.GET("login/", LoginView)
 	err := router.Run(":8080")
 	if err != nil {
 		utils.LogRus.Errorf("Gin启动失败")
