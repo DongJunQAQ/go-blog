9b2d336b8129e082dc42527ec4b4076d95d5ed9b
DongJun <you@example.com>
2024-08-24 14:48:13 +0800

    引入Gin

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"GoBlog/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Home(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "home.html", nil)
+}
+func main() {
+	router := gin.Default()
+	router.LoadHTMLFiles("view/home.html") //加载HTML文件
+	router.GET("/", Home)
+	err := router.Run(":8080")
+	if err != nil {
+		utils.LogRus.Errorf("Gin启动失败")
+	}
+}
