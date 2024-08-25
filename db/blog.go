ee0f086c74a57eddb5505adece45ed67e38559f3
DongJun <you@example.com>
2024-08-25 15:48:59 +0800

    博客详情页面

--- db/blog.go
+++ db/blog.go
@@ -11,7 +11,7 @@ import (
 func GetBlogById(id uint) *models.Blog { //根据博客id查询博客信息
 	db := ConnectMySQL()
 	var blog models.Blog
-	if err := db.Select("id,user_id,title,article").Where("id = ?", id).First(&blog).Error; err != nil {
+	if err := db.Select("id,title,article,update_time").Where("id = ?", id).First(&blog).Error; err != nil {
 		if !errors.Is(gorm.ErrRecordNotFound, err) {
 			utils.LogRus.Errorf("获取博客%d的数据失败:%s", id, err)
 		}
--- handler/blog.go
+++ handler/blog.go
@@ -8,13 +8,27 @@ import (
 	"strconv"
 )
 
-func BlogListHandler(ctx *gin.Context) {
+func BlogListHandler(ctx *gin.Context) { //获取某用户的博客列表
 	uid, err := strconv.Atoi(ctx.Param("uid")) //获取URL中的参数
 	if err != nil {
-		ctx.String(http.StatusBadRequest, "无效的uid")
+		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "无效的用户id"})
 		return
 	}
 	blogs := db.GetBlogByUserIdList(uint(uid))
 	utils.LogRus.Debugf("用户%d共有%d篇博客", uid, len(blogs))
 	ctx.HTML(http.StatusOK, "blog_list.html", blogs) //给前端返回HTML页面和blogs结构体切片
+}
+func BlogDetailHandler(ctx *gin.Context) { //获取某篇博客的详情
+	bid, err := strconv.Atoi(ctx.Param("bid"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "无效的博客id"})
+		return
+	}
+	blog := db.GetBlogById(uint(bid))
+	if blog == nil {
+		ctx.JSON(http.StatusOK, gin.H{"msg": "该博客不存在"})
+		return
+	}
+	utils.LogRus.Debugf("该博客的正文:%s", blog.Article)
+	ctx.HTML(http.StatusOK, "blog.html", gin.H{"bid": blog.ID, "title": blog.Title, "article": blog.Article, "update_time": blog.UpdateTime.Format("2006-01-02 15:04:05")}) //给前端返回一个map，.Format将日期转换为字符串
 }
