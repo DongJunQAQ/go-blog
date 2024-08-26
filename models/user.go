6f70b6c5a8a11722bcede889bf1da309df57f4ed
DongJun <you@example.com>
2024-08-26 15:01:39 +0800

    添加注释

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement"` //``被称为Tag
+	ID       uint   `gorm:"primaryKey;autoIncrement"` //自动编号从1开始，``内的内容被称为Tag
 	Name     string `gorm:"unique;size:20"`
 	Password string `gorm:"size:32"`
 }
